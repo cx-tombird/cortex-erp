3f61da96e0776bed07330e25a02d18be81eb6615
amb-tombird <ThomasBird@ambassador-textiles.co.uk>
2026-02-03 10:44:21 +0000

    Add HasPermissions trait to User model for role and permission management

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasPermissions, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass-assignable.
